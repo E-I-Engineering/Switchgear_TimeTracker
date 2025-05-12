f87767d66288ff6736ef2dbdff54533e380eed45
bigbadtubadude <cralex0307@gmail.com>
2025-05-12 08:22:12 -0400

    runs sql stored procedure with hard coded values

--- Controllers/LogTimeController.cs
+++ Controllers/LogTimeController.cs
@@ -1,6 +1,8 @@
 ﻿using System.Threading.Tasks;
+using Azure.Core;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using Switchgear_TimeTracker.Data;
 using Switchgear_TimeTracker.Models;
@@ -21,6 +23,21 @@ namespace Switchgear_TimeTracker.Controllers
         {
             var project = await _context.TblProjects.ToListAsync();
             return View(project);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ClockUserInOut(IFormCollection form)
+        {
+            var userClock = form["userClock"].ToString();
+            if (string.IsNullOrWhiteSpace(userClock))
+            {
+                return BadRequest("Missing userClock value.");
+            }
+
+            var clockUserID = new SqlParameter("@clockUserID", userClock);
+            var clockProjectID = new SqlParameter("@clockProjectID", 23);
+            await _context.Database.ExecuteSqlRawAsync("EXECUTE dbo.spClockUserInOut @clockUserID, @clockProjectID", clockUserID, clockProjectID);
+            return RedirectToAction("Index", new { projectId = 23 });
         }
         public async Task<IActionResult> Index(int? projectId)
         {
