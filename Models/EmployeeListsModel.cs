7769eb63ab9bb180608fc0be96cf75742da1cff1
bigbadtubadude <cralex0307@gmail.com>
2025-06-19 09:00:44 -0400

    added working and downed workers table to view employee page

---
+++ Models/EmployeeListsModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Switchgear_TimeTracker.Models
+{
+    public class EmployeeListsModel
+    {
+        public required List<TblLaborTimeStamp> Working {  get; set; }
+        public required List<TblLaborTimeStamp> Down {  get; set; }
+    }
+}
