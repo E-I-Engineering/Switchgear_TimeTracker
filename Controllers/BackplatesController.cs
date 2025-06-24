5a5566798d95cd131b25fde5e2eaba1977a151f8
coleman.alexander@vertiv.com <cralex0307@gmail.com>
2025-06-24 06:15:39 -0400

    Added blank page for backplate create and redirects

---
+++ Controllers/BackplatesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Switchgear_TimeTracker.Controllers
+{
+    public class BackplatesController : Controller
+    {
+        public IActionResult Index(int taskID)
+        {
+            if (taskID == 0)
+                // If panel is not selected, redirect to select task
+            {
+                return RedirectToAction("Index", "LogTime");
+            }
+            return View(taskID);
+        }
+    }
+}
